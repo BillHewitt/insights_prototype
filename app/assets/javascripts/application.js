c64cf813627e8a335038a57bd6a7b8cd1d34105d
Hristo <h.valyavicharski@gmail.com>
2017-12-06 14:18:21 +0200

    Add javascript and content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,52 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
+
+// CORE
+//= require babel-external-helpers/babel-external-helpers
+//= require jquery/jquery
+//= require popper-js/umd/popper.min
+//= require bootstrap/bootstrap
+//= require animsition/animsition
+//= require mousewheel/jquery.mousewheel
+//= require asscrollbar/jquery-asScrollbar
+//= require asscrollable/jquery-asScrollable
+//= require ashoverscroll/jquery-asHoverScroll
+
+// Plugins
+//= require switchery/switchery
+//= require intro-js/intro
+//= require screenfull/screenfull
+//= require slidepanel/jquery-slidePanel
+//= require skycons/skycons
+//= require chartist/chartist.min
+//= require chartist-plugin-tooltip/chartist-plugin-tooltip
+//= require aspieprogress/jquery-asPieProgress.min
+//= require jvectormap/jquery-jvectormap.min
+//= require jvectormap/maps/jquery-jvectormap-au-mill-en
+//= require matchheight/jquery.matchHeight-min
+
+// Scripts
+//= require global/Component
+//= require global/Plugin
+//= require global/Base
+//= require global/Config
+
+//= require base/assets/js/Section/Menubar
+//= require base/assets/js/Section/GridMenu
+//= require base/assets/js/Section/Sidebar
+//= require base/assets/js/Section/PageAside
+//= require base/assets/js/Plugin/menu
+
+//= require global/config/colors
+//= require base/assets/js/config/tour
+
+// Page
+//= require base/assets/js/Site
+//= require global/Plugin/asscrollable
+//= require global/Plugin/slidepanel
+//= require global/Plugin/switchery
+//= require global/Plugin/matchheight
+//= require global/Plugin/jvectormap
+
+//= require base/assets/examples/js/dashboard/v1
